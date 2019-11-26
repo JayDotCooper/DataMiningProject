1ef414f3750a6ed6ea84a31dbc7a7b3bfefe8c7a
JayDotCooper <j.cooper212@yahoo.com>
2019-11-25 20:26:47 -0600

    Added classifier

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, flash
-from forms import SearchForm
+from forms import SearchForm, ClassifierForm
 import numpy
 import json
 
+# Globals
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u7PrtQm5Dur0cgkcyOlOmU7kYm9xy4ng'
 champions = {}
 bucket = []
+
+# Constants
+ASSASSIN_TOTAL = 17.0
+FIGHTER_TOTAL = 39.0
+MAGE_TOTAL = 32.0
+MARKSMAN_TOTAL = 23.0
+SUPPORT_TOTAL = 15.0
+TANK_TOTAL = 19.0
+
 
 # Creating the JSON
 with open('ChampionDataset.json') as json_file:
@@ -29,6 +39,18 @@ def home():
     return render_template('home.html', title="Home")
 
 
+@app.route('/classifier', methods=['GET', 'POST'])
+def classifier():
+    form = ClassifierForm()
+    if form.validate_on_submit():
+        # print('Inputs: {0}\n'.format(form.classifierInput._value()))
+        classifierInputs = form.classifierInput._value().split()
+        roleProbability = classify(classifierInputs)
+        return render_template('classifier.html', title="Classifier", form=form, roleProbability=roleProbability)
+    else:
+        return render_template('classifier.html', title="Classifier", form=form)
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
@@ -43,6 +65,258 @@ def search():
     else:
         flash(f'{form.searchInput} is not valid', 'error')
         return render_template('search.html', title="Search", form=form)
+
+
+def classify(classifierInputs):
+    championCount = len(bucket)
+
+    # Lists for each classifier role
+    assassinList = []
+    fighterList = []
+    mageList = []
+    marksmanList = []
+    supportList = []
+    tankList = []
+
+    # Creating the role lists
+    for champion in champions:
+        role = champion["Role"]
+
+        if role == "FIGHTER":
+            fighterList.append(champion)
+        elif role == "SUPPORT":
+            supportList.append(champion)
+        elif role == "MAGE":
+            mageList.append(champion)
+        elif role == "MARKSMAN":
+            marksmanList.append(champion)
+        elif role == "ASSASSIN":
+            assassinList.append(champion)
+        elif role == "TANK":
+            tankList.append(champion)
+
+    # List to hold the number of occurences for each term, given each role
+    assassinOccurences = []
+    fighterOccurences = []
+    mageOccurences = []
+    marksmanOccurences = []
+    supportOccurences = []
+    tankOccurences = []
+
+    # Keeping count for probabilities at the end
+    assassinCount = len(assassinList)
+    fighterCount = len(fighterList)
+    mageCount = len(mageList)
+    marksmanCount = len(marksmanList)
+    supportCount = len(supportList)
+    tankCount = len(tankList)
+
+    print("Assassing count before: {0}\n".format(assassinCount))
+    # Assassin Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in assassinList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            assassinCount += 1
+            termData["Occurrences"] = 1
+        assassinOccurences.append(termData)
+
+    # Fighter Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in fighterList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            fighterCount += 1
+            termData["Occurrences"] = 1
+
+        fighterOccurences.append(termData)
+
+    # Mage Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in mageList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            mageCount += 1
+            termData["Occurrences"] = 1
+        mageOccurences.append(termData)
+
+    # Marksman Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in marksmanList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            marksmanCount += 1
+            termData["Occurrences"] = 1
+
+        marksmanOccurences.append(termData)
+
+    # Support Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in supportList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            supportCount += 1
+            termData["Occurrences"] = 1
+
+        supportOccurences.append(termData)
+
+    # Tank Loop
+    for term in classifierInputs:
+        occurrences = 0
+        for champion in tankList:
+            if term.upper() in champion["Bio"].upper():
+                occurrences += 1
+
+        termData = {
+            "Term": term,
+            "Occurrences": occurrences
+        }
+
+        # Smoothing
+        if occurrences == 0:
+            championCount += 1
+            tankCount += 1
+            termData["Occurrences"] = 1
+
+        tankOccurences.append(termData)
+
+    probAssassin = 0.0
+    probNotAssassin = 0.0
+    probFighter = 0.0
+    probNotFighter = 0.0
+    probMage = 0.0
+    probNotMage = 0.0
+    probMarksman = 0.0
+    probNotMarksman = 0.0
+    probSupport = 0.0
+    probNotSupport = 0.0
+    probTank = 0.0
+    probNotTank = 0.0
+
+    # Assassin Probability Calcuations
+    for termData in assassinOccurences:
+        probAssassin += (termData["Occurrences"] / assassinCount)
+    for (f, mage, mark, supp, tank) in zip(fighterOccurences, mageOccurences, marksmanOccurences, supportOccurences, tankOccurences):
+        probNotAssassin += ((f["Occurrences"] + mage["Occurrences"] + mark["Occurrences"] + supp["Occurrences"] + tank["Occurrences"])/(championCount - assassinCount))
+
+    probAssassin += (assassinCount / championCount)
+    probNotAssassin += ((championCount - assassinCount) / championCount)
+    probAssassin = probAssassin / probNotAssassin
+
+    # Fighter Probability Calcuations
+    for termData in fighterOccurences:
+        probFighter += (termData["Occurrences"] / fighterCount)
+    for (ass, mage, mark, supp, tank) in zip(assassinOccurences, mageOccurences, marksmanOccurences, supportOccurences, tankOccurences):
+        probNotFighter += ((ass["Occurrences"] + mage["Occurrences"] + mark["Occurrences"] + supp["Occurrences"] + tank["Occurrences"]) / (championCount - fighterCount))
+
+    probFighter += (fighterCount / championCount)
+    probNotFighter += ((championCount - fighterCount) / championCount)
+    probFighter = probFighter / probNotFighter;
+
+    # Mage Probability Calcuations
+    for termData in mageOccurences:
+        probMage += (termData["Occurrences"] / mageCount)
+    for (f, ass, mark, supp, tank) in zip(fighterOccurences, assassinOccurences, marksmanOccurences, supportOccurences, tankOccurences):
+        probNotMage += ((f["Occurrences"] + ass["Occurrences"] + mark["Occurrences"] + supp["Occurrences"] + tank["Occurrences"]) / (championCount - mageCount))
+
+    probMage += (mageCount / championCount)
+    probNotMage += ((championCount - mageCount) / championCount)
+    probMage = probMage / probNotMage
+
+    # Marksman Probability Calcuations
+    for termData in marksmanOccurences:
+        probMarksman += (termData["Occurrences"] / marksmanCount)
+    for (f, mage, ass, supp, tank) in zip(fighterOccurences, mageOccurences, assassinOccurences, supportOccurences, tankOccurences):
+        probNotMarksman += ((f["Occurrences"] + mage["Occurrences"] + ass["Occurrences"] + supp["Occurrences"] + tank["Occurrences"]) / (championCount - marksmanCount))
+
+    probMarksman += (marksmanCount / championCount)
+    probNotMarksman += ((championCount - marksmanCount) / championCount)
+    probMarksman = probMarksman / probNotMarksman
+
+    # Support Probability Calcuations
+    for termData in supportOccurences:
+        probSupport += (termData["Occurrences"] / supportCount)
+    for (f, mage, mark, ass, tank) in zip(fighterOccurences, mageOccurences, marksmanOccurences, assassinOccurences, tankOccurences):
+        probNotSupport += ((f["Occurrences"] + mage["Occurrences"] + mark["Occurrences"] + ass["Occurrences"] + tank["Occurrences"]) / (championCount - supportCount))
+
+    probSupport += (supportCount / championCount)
+    probNotSupport += ((championCount - supportCount) / championCount)
+    probSupport = probSupport / probNotSupport
+
+    # Tank Probability Calcuations
+    for termData in tankOccurences:
+        probTank += (termData["Occurrences"] / tankCount)
+    for (f, mage, mark, supp, ass) in zip(fighterOccurences, mageOccurences, assassinOccurences, supportOccurences, tankOccurences):
+        probNotTank += ((f["Occurrences"] + mage["Occurrences"] + mark["Occurrences"] + supp["Occurrences"] + ass["Occurrences"]) / (championCount - tankCount))
+
+    probTank += (tankCount / championCount)
+    probNotTank += ((championCount - tankCount) / championCount)
+    probTank = probTank / probNotTank
+
+    # Normalizing probabilities to add up to 100%
+    probTotal = probAssassin + probFighter + probMage + probMarksman + probSupport + probTank
+    probAssassin = (probAssassin / probTotal) * 100
+    probFighter = (probFighter / probTotal) * 100
+    probMage = (probMage / probTotal) * 100
+    probMarksman = (probMarksman / probTotal) * 100
+    probSupport = (probSupport / probTotal) * 100
+    probTank = (probTank / probTotal) * 100
+
+    return {"Assassin": probAssassin,
+            "Fighter": probFighter,
+            "Mage": probMage,
+            "Marksman": probMarksman,
+            "Support": probSupport,
+            "Tank": probTank}
 
 
 def create_tf_matrix(searchInputs):
--- forms.py
+++ forms.py
@@ -7,3 +7,9 @@ class SearchForm(FlaskForm):
     searchInput = StringField('SearchInput',
                               validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField('Go!')
+
+
+class ClassifierForm(FlaskForm):
+    classifierInput = StringField('ClassifierInput',
+                                  validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Go!')
